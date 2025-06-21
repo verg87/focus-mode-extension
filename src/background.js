a18b705b6944d5ac2b9e373200682ffd7a8267f8
victo <victoryellow300@gmail.com>
2025-06-21 19:59:27 +0500

    changed how blocked sites are stored in chrome.storage. added words div in html

--- src/background.js
+++ src/background.js
@@ -14,27 +14,19 @@ const trimUrl = (url) => {
 }
 
 const setInitialValues = () => {
-    chrome.storage.sync.set({
-        "www.youtube.com": false,
-        "discord.com": false,
-        "www.twitch.tv": false,
-        "fr-fr.facebook.com": false,
-    });
+    chrome.storage.sync.set({sites: [
+        "www.youtube.com",
+        "discord.com",
+        "www.twitch.tv",
+        "fr-fr.facebook.com",
+    ]});
 }
 
 const sendMessageToContentScript = async (tabId, url) => {
-    const storage = await chrome.storage.sync.get(null);
-    const sites = Object.keys(storage).filter(key => key !== "isBlocking");
-    let block = false;
+    const { sites } = await chrome.storage.sync.get('sites');
+    const { isBlocking } = await chrome.storage.sync.get('isBlocking');
 
-    try {
-        block = await chrome.storage.sync.get([url]);
-        block = block[url];
-    } catch(e) {
-        console.log(e);
-    }
-
-    if (url && sites.includes(url) && block) {
+    if (url && sites.includes(url) && isBlocking) {
         chrome.tabs.sendMessage(tabId, {
             type: "BLOCK",
             message: url,
@@ -48,13 +40,14 @@ const sendMessageToContentScript = async (tabId, url) => {
     }
 }
 
-const listenForBlockedPage = async (checked, hostname) => {
+const listenForBlockedPage = async (checked, deleted, added) => {
     const tab = await getCurrentTab();
     const currentTabHostname = trimUrl(tab['url']);
+    const { sites } = await chrome.storage.sync.get('sites');
 
-    if (!checked && hostname === currentTabHostname) {
+    if ((!checked && sites.includes(currentTabHostname)) || (!checked && deleted)) { // Doesn't work
         chrome.tabs.reload(tab.id);
-    } else if (checked && hostname === currentTabHostname) {
+    } else if ((checked && sites.includes(currentTabHostname)) || (checked && added)) {
         sendMessageToContentScript(tab.id, currentTabHostname);
     }
 }
@@ -71,20 +64,37 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     } 
 });
 
-chrome.storage.sync.onChanged.addListener((changes) => {
+chrome.storage.sync.onChanged.addListener(async (changes) => {
     const changedKeys = Object.keys(changes);
+    console.log(changes);
     if (changedKeys.length === 0) return; 
   
     const url = changedKeys[0];
     const { oldValue, newValue } = changes[url] || {};
+    let deleted, added;
+    
+    // Doesn't work
+    if (Array.isArray(oldValue) && Array.isArray(newValue)) {
+        if (oldValue.length < newValue.length) {
+            added = true;
+            deleted = false;
+        } else {
+            added = false;
+            deleted = true;
+        }
+    }
 
     //* listenForBlockedPage should be only called when:
     //* either newValue is true and oldValue is false/undefiend or the other way around
     if (newValue && !oldValue) {
-        listenForBlockedPage(true, url);
+        listenForBlockedPage(true, deleted, added);
     } else if (oldValue && !newValue) {
-        listenForBlockedPage(false, url);
-  }
+        listenForBlockedPage(false, deleted, added);
+    }
+
+    // const { isBlocking } = await chrome.storage.sync.get('isBlocking');
+    
+    // listenForBlockedPage(isBlocking, sites);
 });
 
 chrome.runtime.onInstalled.addListener((details) => {
